cff45e8fe928637869bc547ec06de733959306ad
dora kojevnikov <feodora.k@gmail.com>
2022-08-01 10:26:29 +1000

    card controller added. whitelist updated

--- api/server.js
+++ api/server.js
@@ -5,6 +5,13 @@ const mongoose = require("mongoose")
 const app = express()
 const PORT = process.env.PORT
 const dbURL = process.env.MONGODB_URL
+const whitelist = ['http://localhost:3001']
+
+const cardsController = require("./controllers/cards")
+
+app.use(express.json())
+
+app.use("/cards", cardsController)
 
 mongoose.connect(dbURL, () => {
   console.log("connected to jello db")
