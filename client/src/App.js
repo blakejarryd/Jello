894ff0692b5e5420b89bc0087b7787ce1d13ebf7
blakejarryd <blakejarrydjohnston@gmail.com>
2022-08-01 16:22:59 +1000

    delete functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,21 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import Board from './components/Board'
 import CardDetail from './components/CardDetails';
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
+  const handleDelete = async (id) => {
+    console.log(id)
+    const url = `http://localhost:3000/cards/${id}`
+    await fetch(url, {
+      method: 'DELETE'
+    })
+  }
+
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Board />} />
-        <Route path='/:id' element={<CardDetail />} />
+        <Route path='/:id' element={<CardDetail handleDelete={handleDelete}/>} />
       </Routes>
     </div>
   );
